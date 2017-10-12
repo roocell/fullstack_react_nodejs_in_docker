fbeea0a41ff66da59bbccace5a28cc36d383ff93
Michael Russell <roocell@gmail.com>
2017-10-11 23:03:31 -0400

    async, await, Promises... oh my. user registration API working. asyncronous code is fun.

--- api/routes/db.js
+++ api/routes/db.js
@@ -2,15 +2,39 @@
 // https://github.com/mysqljs/mysql
 //var mysql = require('mysql');
 
-module.exports = {
-  info: function() {
-    var credentials = {
-      host     : 'mysql', // from the --link option in docker
-      user     : 'dbuser',
-      password : 'admin123',
-      database : 'teleport'
-    };
-    return credentials;
-  }
+// Promisify mysql so we can use Promises instead of callbacks
+// https://codeburst.io/node-js-mysql-and-promises-4c3be599909b
 
-};
+// can also use Promisify (which is part of nodejs in 2015)
+// https://codeburst.io/quick-dig-promisify-in-node-js-6d5d763f847d
+
+const mysql = require( 'mysql' );
+module.exports = class Database {
+    constructor( ) {
+      var config = {
+        host     : 'mysql', // from the --link option in docker
+        user     : 'dbuser',
+        password : 'admin123',
+        database : 'teleport'
+      };
+      this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,60 +1,47 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./db');
-var mysql = require('mysql');
+const Database = require('./db');
 
 // test post by command line - this will be similar to what we'll send from the ios app
 // curl -d '{"action":"register", "userid":"abc123"}' -H "Content-Type: application/json" http://192.168.99.100:3001/users
 
-// https://codeburst.io/node-js-mysql-and-promises-4c3be599909b
+const checkAndRegisterUser = async (userid) => {
+  var database = new Database();
+  var response = {status: "fail2"};
 
-const registerUser = async (userid) => {
-  var response = {status: "fail"};
-  var connection = mysql.createConnection(db.info());
-  connection.connect();
-
-  try {
-    var query = "INSERT INTO users (userid) VALUES ('" + userid +"') ";
-    console.log(query);
-    var results = await connection.query(query);
-    response = {status: "pass", reason: "added"};
-    return response;
-  } catch (err) {
-    console.log(err);
-    response = {status: "fail", reason: "db_error2"};
-  }
-  return response;
-}
-
-const checkUserExists = async (userid) => {
-  var response = {status: "fail"};
-  var connection = mysql.createConnection(db.info());
-  connection.connect();
-
-  try {
-    var query = "SELECT COUNT(*) as count FROM users WHERE userid = '" + userid + "' ";
-    console.log(query);
-    var results = await connection.query(query);
-    console.log(results);
-    if (results[0].count > 0)
+  var query = "SELECT COUNT(*) as count FROM users WHERE userid = '" + userid + "' ";
+  await database.query(query)  // need to await here so the return at the end of the func wait for all this to happen
+  .then( rows => {
+    // do something with the result
+    //console.log(rows);
+    if (rows[0].count > 0)
     {
       console.log("you exist");
       response = {status: "pass", reason: "exists"};
+    } else {
+      console.log("inserting");
+      var query = "INSERT INTO users (userid) VALUES ('" + userid +"') ";
+      return database.query(query);
     }
-    return response;
-  } catch (err) {
-    response = {status: "fail", reason: "db_error1"};
-  }
-  return response;
-}
+  }, err => {
+    response = {status: "pass", reason: "db_err_select"};
+    return database.close().then( () => { throw err; } )
+  })
+  .then( rows => {
+    if (response.reason != "exists")
+    {
+      response = {status: "pass", reason: "added"};
+    }
+    return database.close();
+  }, err => {
+    response = {status: "pass", reason: "db_err_insert"};
+    return database.close().then( () => { throw err; } )
+  })
+  .catch( err => {
+          // handle the error
+  } )
 
-const checkAndRegisterUser = async (userid) => {
-  var response = await checkUserExists(userid);
-  if (response.reason == "exists")
-  {
-    return response;
-  }
-  return await registerUser(userid);
+  return response;
 }
 
 router.post('/', async function(req, res) {
@@ -69,6 +56,7 @@ router.post('/', async function(req, res) {
        console.log("/users, unhandled action" + req.body.action);
        response = {status: "fail", reason: "unhandled action"};
    }
+   console.log(response);
    res.json(response);
 });
 
